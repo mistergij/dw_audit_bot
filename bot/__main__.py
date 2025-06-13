7d17ea728736044ceaa1264323120ac369647e03
Infinidoge <infinidoge@inx.moe>
2025-06-13 18:52:37 -0400

    Nixify package

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,18 +1,22 @@
 """Main entry point for the audit bot."""
+
 import crescent
-import dotenv
 import hikari
 
 from bot.audit_model import AuditModel
 from bot.utils import DISCORD_TOKEN
 
-dotenv.load_dotenv()
-
-audit_bot = hikari.GatewayBot(DISCORD_TOKEN) # pyright: ignore
+audit_bot = hikari.GatewayBot(DISCORD_TOKEN)  # pyright: ignore
 
 audit_model = AuditModel()
 
 client = crescent.Client(audit_bot, audit_model)
 client.plugins.load_folder("bot.plugins")
 
-audit_bot.run()
+
+def main():
+    audit_bot.run()
+
+
+if __name__ == "__main__":
+    main()

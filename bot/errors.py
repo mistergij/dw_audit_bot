141076f1282dbb03118ba4f3718854449b2a8836
mistergij <57270253+mistergij@users.noreply.github.com>
2025-08-12 15:58:57 -0400

    Add License to errors.py

--- bot/errors.py
+++ bot/errors.py
@@ -1,8 +1,23 @@
+"""Plugin that contains all audit commands for the bot.
+Copyright © 2025 Dnd World
+
+This file is part of Kensa.
+Kensa is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+Kensa is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with Kensa. If not, see
+<https://www.gnu.org/licenses/>.
+"""
+
 from typing import override
 
 import crescent
 
-from bot import client
 from bot.constants import Plugin
 
 
